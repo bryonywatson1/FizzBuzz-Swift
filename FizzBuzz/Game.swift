e5b1499e2031950c9aa87c3f6ed28d1c457409b9
Courtney Osborn <Courtney.lee.osborn@gmail.com>
2017-01-30 15:44:16 +0000

    Adds game file

---
+++ FizzBuzz/Game.swift
@@ -0,0 +1,13 @@
+//
+//  Game.swift
+//  FizzBuzz
+//
+//  Created by Courtney Osborn on 30/01/2017.
+//  Copyright © 2017 Courtney Osborn. All rights reserved.
+//
+
+import Foundation
+
+class Game: NSObject {
+    
+}
---
+++ FizzBuzzTests/GameTests.swift
@@ -0,0 +1,26 @@
+//
+//  GameTests.swift
+//  FizzBuzz
+//
+//  Created by Courtney Osborn on 30/01/2017.
+//  Copyright © 2017 Courtney Osborn. All rights reserved.
+//
+
+import XCTest
+
+@testable import FizzBuzz
+
+class GameTests: XCTestCase {
+    
+    let game = Game()
+    
+    override func setUp() {
+        super.setUp()
+            }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    
+}
